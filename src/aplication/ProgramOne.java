c09d41c488eefa228f6ca356755a7f8e384f7b45
Caio Ramos <caio_ramos_abc@hotmail.com>
2021-04-22 20:58:46 -0300

    Projeto Criado

---
+++ src/aplication/ProgramOne.java
@@ -0,0 +1,30 @@
+package aplication;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class ProgramOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy HH:mm:ss");
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		
+		System.out.println(sdf.format(d));
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		//cal.add(Calendar.HOUR_OF_DAY, 4);
+		//d = cal.getTime();
+		int minutes = cal.get(Calendar.MINUTE);
+		int month = cal.get(Calendar.MONTH);
+		
+		
+		System.out.println("Minutes : " + minutes);
+		System.out.println("Month : " + month);
+		
+	}
+
+}
